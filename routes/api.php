df80f9d36638dc282999e61566e08997245731df
Heriberto Martinez <heriberto@MacBook-Pro-de-Heriberto.local>
2018-01-03 22:22:56 -0600

    Se implementa el envio de email

--- routes/api.php
+++ routes/api.php
@@ -112,3 +112,5 @@ Route::resource('sellers.products','Seller\SellerProductController',[
 Route::resource('users','User\UserController',[
     'except' => ['create', 'edit']
 ]);
+Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
+Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
