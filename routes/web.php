ed1d60f75fedbc8c8de7a272acb16a0ccb3ba3c3
HeribertoME <hmelizarraraz@hotmail.com>
2018-01-09 14:20:13 -0600

    Se implementa passport

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+Route::get('/home/authorized-clients', 'HomeController@AuthorizedClients')->name('authorized-clients');
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', function () {
     return view('welcome');
 })->middleware('guest');
